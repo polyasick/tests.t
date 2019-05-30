1559e1506a193589a42304d1bef8e0291bd986c3
polyasick <50992284+polyasick@users.noreply.github.com>
2019-05-30 17:50:54 +0300

    Update quest.php

--- quest.php
+++ quest.php
@@ -2,16 +2,13 @@
 <html lang="ru">
 <head>
 <?php
-
 require_once 'mysqlconnect.php';
 $sql = 'SELECT * FROM `test_list` WHERE `Id_test` = :Id_test';
 $Id = $_GET['Id_test_q'];
 $query = $pdo->prepare($sql);
 $query->execute(['Id_test' => $Id]);
-
 $article = $query->fetch(PDO::FETCH_OBJ);
 $website_title = $article->Name_test;
-
 require 'blocks/head.php'; ?>
 </head>
 <body>
@@ -43,82 +40,110 @@ require 'blocks/head.php'; ?>
        $query->execute(['login' => $username, 'Id_test' =>$Id]);
        $row = $query -> fetch(PDO::FETCH_OBJ);
 	  $id_student = $row-> Id_student;
+	  	$n_popitki = $_GET['npopit'];
 	 //echo $id_student;
-
 // запрос на ввод данных в seans
-   // $sql = "INSERT INTO seans(Id_cl, Id_stud, Id_tests) VALUES($row->Id_class, $row->Id_student,$row->Id_test)";
-     // $query1 = $pdo->query($sql);
 
 if ($_POST['butt']) {
 //очищаем предыдущие результаты
-$sql = "DELETE FROM `concrete_answer` WHERE id_student='". $id_student."' AND Id_test_new='".$Id."'";
-$pdo->query($sql);
-
+//$sql = "DELETE FROM `concrete_answer` WHERE id_student='". $id_student."' AND Id_test_new='".$Id."'";
+//$pdo->query($sql);
 //заносим новые
 	for ($i=0;$i<100;$i++) {
 if 	($_POST['v'.$i]){
+	//echo "lkl";
+
 $pieces = explode("|", $_POST['v'.$i]);
-echo $_POST['v'.$i]."<br/>";
-$sql = "INSERT INTO concrete_answer (Id_note_answ, Id_test_new, Id_question_concr, amswer_stud, id_student) VALUES(NULL, '".$Id."', '".$pieces[0]."', '".$pieces[1]."', '".$id_student."')";
+//echo $_POST['v'.$i]."<br/>";
+$sql = "INSERT INTO concrete_answer (Id_note_answ, Id_test_new, Id_question_concr, amswer_stud, id_student, n_popitki) VALUES(NULL, '".$Id."', '".$pieces[0]."', '".$pieces[1]."', '".$id_student."', '".$n_popitki."')";
 $query1 = $pdo->query($sql);
 }
 }
-
-
-echo "Данные теста успешно занесены!";
+//echo "Данные теста успешно занесены!";
 }
-else {
+
+
  ?>
+ <div>
  <div class= "jumbotron">
-<form method="post" action="">
-<?php
-//номер вопроса
+   <form method="post" action="">
+      <?php
+      //получаем вопросы, на которые уже дан ответ
+      $chet_otv = -1; // счетчик отвеченных
+      $sql = "SELECT `Id_question_concr` FROM `concrete_answer` WHERE id_student='". $id_student."' AND Id_test_new='".$Id."' AND n_popitki='".$n_popitki."'";
+      foreach ($pdo->query($sql) as $row_u) {
+      $chet_otv ++;
+      $mass_proid[$chet_otv] = $row_u['Id_question_concr'];
+      }
 
+      if ($n_popitki <4) {
 
-$quest = 'SELECT  question.Text_quest, question.Id_question
-from `question`
-join `test_list`
-on question.Id_tests = test_list.Id_test
-where `Id_test` = :Id_test ';
+          if ($chet_otv<4) {
+            if($chet_otv == 0)
+            {$sql = "INSERT INTO seans(Id_cl, Id_stud, Id_tests) VALUES($row->Id_class, $row->Id_student,$row->Id_test)";
+               $query1 = $pdo->query($sql);}
+          //номер вопроса
+          $quest = 'SELECT  question.Text_quest, question.Id_question
+          from `question`
+          join `test_list`
+          on question.Id_tests = test_list.Id_test
+          where `Id_test` = :Id_test ';
+          $query = $pdo->prepare($quest);
+          $query->execute(['Id_test' =>$Id]);
+          $chet=-1;
 
-$query = $pdo->prepare($quest);
-$query->execute(['Id_test' =>$Id]);
+          //создаем и заполняем массив вопросов
+          $question_mass_text = array();
+          $question_mass_id  = array();
+          while ($row = $query -> fetch(PDO::FETCH_OBJ) )
+          {
+          $flg = true;
+          	for ($i=0;$i<=$chet_otv;$i++) {
+              if ($mass_proid[$i]==$row->Id_question)
+                  $flg = false;
+          	}
 
+          if ($flg) {
+            $chet++;
+            $question_mass_text[$chet] = $row->Text_quest;
+            $question_mass_id [$chet] =$row->Id_question;
+            }
+          }
 
-$chet=-1;
-$question_mass_text = array();
-$question_mass_id  = array();
-while ($row = $query -> fetch(PDO::FETCH_OBJ) )
-{
-$chet++;
-$question_mass_text[$chet] = $row->Text_quest;
-$question_mass_id [$chet] =$row->Id_question;
-}
+        //выбираем случайный вопрос
+        $rnd = rand(0, $chet);
 
-for ($i=0; $i<=$chet;$i++) {
-echo "Вопрос №".($i+1)." <br />".$question_mass_text[$i]."<br /><br />Ответы: <br />";
-
-$Id_que = $question_mass_id [$i];
-
-$answ = "SELECT * FROM `list_answer` where `Id_question_LAnsw` = '".$Id_que."'";
-$query = $pdo->prepare($answ);
-$query->execute(['Id_answer' =>$Id]);
-while ($row = $query -> fetch(PDO::FETCH_OBJ) )
-{
-	echo "<input  class='form-check-input' type='radio' name='v ".$i."' value='".$row->Id_question_LAnsw."|".$row->answer."'>".$row->answer."<br />";
-}
-
-echo "<br />";
-echo '<input type="hidden" value = "'.$Id.'" name="Id_test_new"/>';
-}
+        echo "<h4>Вопрос №".($i+1)."</h4>"." <br />"."<h6>".$question_mass_text[$rnd]."</h6>"."<br />Ответы: <br />";
+        $Id_que = $question_mass_id [$rnd];
+        $answ = "SELECT * FROM `list_answer` where `Id_question_LAnsw` = '".$Id_que."'";
+        $query = $pdo->prepare($answ);
+        $query->execute(['Id_answer' =>$Id]);
+        while ($row = $query -> fetch(PDO::FETCH_OBJ) )
+        {
+        	echo "<input  class='form-check-input' type='radio' name='v".$i."' value='".$row->Id_question_LAnsw."|".$row->answer."'>".$row->answer."<br />";
+        }
+        echo "<br />";
+        echo '<input type="hidden" value = "'.$Id.'" name="Id_test_new"/>';
 
         ?>
-<input type="submit" value = "Я решил!" class="btn btn-secondary mt-3" name="butt"/>
-</form>
+        </div>
+        <input type="submit" value = "Следующий вопрос" class="btn btn-secondary mt-3" name="butt"/>
+        </form>
       </div>
 
+
     </div>
-<?php }?>
+<?php
+}
+
+else {
+echo "Список ваших вопросов:";
+
+}
+}
+
+else echo "Вы прошли этот тест 3 раза!";
+?>
 <!--
     <aside class="col-md-4">
       <div class = "p-3 mb-4" style="background-color: #98A4B3">
@@ -128,7 +153,6 @@ echo '<input type="hidden" value = "'.$Id.'" name="Id_test_new"/>';
           Нажмите на тест, тогда увидите свои оценки.
       </p>
        </div>
-
      </aside>-->
 
    </div>
